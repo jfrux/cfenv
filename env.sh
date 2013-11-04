bd5215690bd649b0b65d07683c31aed29f16608a
Joshua Rountree <joshua@swodev.com>
2013-11-04 15:15:04 -0500

    fixes

--- env.sh
+++ env.sh
@@ -10,18 +10,4 @@ set +u
 export CFENV_ROOT=$HOME/.cfenv
 
 # Add railo'd stuff to the path.
-PATH=$CFENV_ROOT:$PATH
-
-# FOR MORE GOODIES LATER...
-# for f in $CFENV_ROOT/env.d/*.sh ; do
-#   if [ -f $f ] ; then
-#     source $f
-#   fi
-# done
-
-# Cfenv is installed.
-if [ -d "$CFENV_ROOT/.git" ]; then
-  export CFENV_SETUP_VERSION=`GIT_DIR=$CFENV_ROOT/.git git rev-parse --short HEAD`
-else
-  echo "cfenv could not load properly!"
-fi
+PATH=$CFENV_ROOT/libexec:$PATH
